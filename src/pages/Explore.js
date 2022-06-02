64825cf955a3deaa222fd818d0dada088dfe790e
heejun Shin <shin970421@gmail.com>
2022-06-02 17:16:58 +0900

    explore rendering 변경

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -5,7 +5,6 @@ import logo from '../logo.svg';
 import { Carousel,Navbar,Container, Nav, NavDropdown, Card,DropdownButton, Dropdown } from 'react-bootstrap';
 import { BrowserRouter, Route, Routes, Link, useNavigate} from 'react-router-dom';
 
-import axios from 'axios';
 import './/pagecss/Explore.css';
 import {ACCESS_KEY_ID, SECRET_ACCESS_KEY} from '../apikey';
 import Caver from 'caver-js';
@@ -24,58 +23,52 @@ const option = {
   ]
 }
 
-let caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn",option));
+const caver = new Caver(new Caver.providers.HttpProvider("https://node-api.klaytnapi.com/v1/klaytn",option));
 const NFTContract = new caver.contract(ABI, CONTRACTADDRESS);
 
-// const TotalNFT = async () => {
-//     const total = await NFTContract.methods.totalSupply().call();
-//     console.log(total);
-//     var tokenIDs = []
-//     var tokenURIS = []
-//     var tokenJson = []
-//     var nfts = []
-//     for (var i = 0; i<total; i++){
-//         const nftIndex = await NFTContract.methods.tokenByIndex(i).call();
-//         tokenIDs.push(nftIndex);
-//     }
-//     for (var i = 0; i < tokenIDs.length; i++){
-//         const nftURI = await NFTContract.methods.tokenURI(tokenIDs[i]).call();
-//         tokenURIS.push(nftURI);
-//     }
+const TotalNFT = async () => {
+    const total = await NFTContract.methods.totalSupply().call();
+    console.log(total);
+    var tokenIDs = []
+    var animalName = []
+    var tokenURIs = []
+    var tokenimg = []
+    var nftprice = []
+    var nfts = []
+    for (var i = 0; i<total; i++){
+        // 토큰 ID
+        const nftIndex = await NFTContract.methods.tokenByIndex(i).call();
+        tokenIDs.push(nftIndex);
+    }
 
-//     for (var i = 0; i < tokenURIS.length+1; i++){
-//         fetch(tokenURIS[i])
-//         .then(res => res.json())
-//         .then((out) => {
-//             tokenJson.push(out.image)
-//         })
-//     }
+    for (var i = 0; i<tokenIDs.length; i++){
+        // NFT 가격
+        const nftPrice = await NFTContract.methods.animalTokenPrices(tokenIDs[i]).call();
+        nftprice.push(nftPrice);
+        // NFT 이미지
+        const nftImg = await NFTContract.methods.tokenURI(tokenIDs[i]).call();
+        fetch(nftImg)
+        .then(res => res.json())
+        .then((out) => {
+            tokenimg.push(out.image)
+        })
+        // NFT 이름
+        const nftNm = await NFTContract.methods.tokenURI(tokenIDs[i]).call();
+        fetch(nftNm)
+        .then(res => res.json())
+        .then((out) => {
+            animalName.push(out.attributes[1].value)
+        })
+        // NFT JSON URI
+        const nftURI = await NFTContract.methods.tokenURI(tokenIDs[i]).call();
+        tokenURIs.push(nftURI);
 
-//     // var request = new XMLHttpRequest();
-//     // for (var i = 0; i < tokenURIS.length; i++){
-//     //     request.open('GET', tokenURIS[i]);
-//     //     request.responseType = 'json';
-//     //     request.send();
-//     //     request.onload = function() {
-//     //         var jsontext = request.response;
-//     //         tokenJson.push(jsontext);
-//     //     }
-//     // }
+        nfts.push({id : tokenIDs[i], uri: tokenURIs[i], image: tokenimg[i], name: animalName[i], price: nftprice[i]});
 
-//     for (var i = 0; i < tokenIDs.length; i++){
-//         nfts.push({id : tokenIDs[i], uri: tokenURIS[i]});
-//     }
-//     console.log(tokenJson);
-//     console.log(nfts);
-//     return nfts;
-// }
-
-async function GetInfo(jsonAddress){
-    let Data = await axios.get(jsonAddress);
-    let str = JSON.stringify(Data);
-    let jsonArray = await JSON.parse(str);
-    let res = jsonArray.data
-    return res;
+    }
+    
+    console.log(nfts);
+    return nfts;
 }
 
 const RankItem = [
@@ -112,39 +105,23 @@ const RankItem = [
 ]
 
 
-function NFTList({NFT_url,NFT_number,NFT_price}){
-    let [data,setData] = useState(null);
-    let [att, setAtt] = useState(null);
+function NFTList({NFT_name,NFT_url,NFT_number,NFT_price}){ 
     let cost = NFT_price/(10**18);
     let [show, setShow] = useState(true);
 
     useEffect(()=>{
-        const info = async()=>{
-            const arr = await GetInfo(NFT_url);
-            setData(arr);
-            const arr1 = Object.values(arr.attributes);
-            console.log(arr1);
-            setAtt(arr1[1].value);
-        }
-        info(); 
         if (cost == 0){
             setShow(false);
         }
     },[]);
 
-   
-
-    if(!data || !att){
-        return null;
-    }
-    
     return(
         <div className='marketImg'>
             <Card className='marketCard'>
-                <Card.Img variant="bottom" src={data.image}/>
+                <Card.Img variant="bottom" src={NFT_url} />
                 <Card.Body className='marketCardBody'>
                     <Card.Text style={{fontWeight:"bold",fontSize:"30px",textAlign:"left"}}>
-                        {att}
+                        {NFT_name}
                         <br/>
                         #{NFT_number}
                     </Card.Text>
@@ -176,108 +153,12 @@ function Ranking({NFT_name,NFT_url}){
 };
 
 function Explore() {
-    const [nfts, setNfts] = useState(null);
+    const [nfts, setNfts] = useState([]);
 
-    // const allnfts = async () => {
-    //     const _nfts = await TotalNFT();
-    //     setNfts(_nfts);
-    // }
-
-    let myContract;
-
-    let accounts;
-    let account;
-    let [TotalObject,setToken_Total] = useState(null);
-    let [SaleObject, setSale_Object] = useState([]);
-
-    async function check_onsale(){
-        accounts = await window.klaytn.enable();
-        account = accounts[0];
-
-
-        var last = 0;
-        var token_sale = [];
-        var index = 0;
-        myContract = new caver.contract(ABI, CONTRACTADDRESS);
-        var token_temp = [];
-
-        await myContract.methods.getOnSaleAnimalTokenArrayLength().call() // 전체 NFT 개수 확인
-            .then(function(result){
-                last = result;
-        });
-        while(index<last){
-            await myContract.methods.onSaleAnimalTokenArray(index).call()
-            .then(function(result){
-                token_temp.push(result);
-                index += 1;
-            })
-            .catch(function (error){
-                console.log(error)
-            });
-        }
-        for (var i of token_temp){
-            var token_one = new Object();
-            await myContract.methods.tokenURI(i).call()
-            .then(function(result){
-                
-                token_one['T_ID'] = i;
-                token_one['T_URI'] = result;
-                
-            });
-
-            await myContract.methods.animalTokenPrices(i).call()
-            .then(function(result){
-                token_one['T_Price'] = result;
-                token_sale.push(token_one);
-            })
-        }
-        setSale_Object(token_sale);
-        // console.log(SaleObject);
-
-
+    const allnfts = async () => {
+        const _nfts = await TotalNFT();
+        setNfts(_nfts);
     }
-
-    async function check_total(){
-
-        accounts = await window.klaytn.enable();
-        account = accounts[0];
-        myContract = new caver.contract(ABI, CONTRACTADDRESS);
- 
-        
-
-        // 전체 NFT 확인
-
-        var last = 0;
-        let token_object = [];
-
-        await myContract.methods.totalSupply().call() // 전체 NFT 개수 확인
-            .then(function(result){
-                last = result;
-        });
-
-    
-
-        for (var i =1; i<= last; i++){
-            var token_one = new Object();
-            await myContract.methods.tokenURI(i).call()
-            .then(function(result){
-                
-                token_one['T_ID'] = i;
-                token_one['T_URI'] = result;
-                
-            });
-            await myContract.methods.animalTokenPrices(i).call()
-            .then(function(result){
-                token_one['T_Price'] = result;
-                token_object.push(token_one);
-            });
-        }
-        setToken_Total(token_object);
-    }
-    check_total(); // TotalObject 에 담김
-    check_onsale(); // SaleObject 에 담김
-    
-    
 
     function numberSort(){
         
@@ -289,13 +170,8 @@ function Explore() {
         
     }
 
-    if(!TotalObject){
-        return null;
-    }
-
     return (
         <>
-        
         <div className='exploreBack'>
             <div className='rankingBack'>
                 <div className='rankingTxt' style={{textAlign:"left",fontWeight:"bold"}}>
@@ -311,8 +187,9 @@ function Explore() {
                 </div>
             </div>
             <div className='marketBack'>
-                <div className='marketTxt' style={{textAlign:"left",fontWeight:"bold"}}>
+                <div className='marketTxt' style={{textAlign:"left",fontWeight:"bold"}} onClick={allnfts()}>
                     MARKET
+                    {/* <button onClick={allnfts()}></button> */}
                 </div>
                 <div className="sortBack">
                     <DropdownButton id="marketSort" variant='light' size='larger' title="Sort By" textAlign="right">
@@ -322,10 +199,15 @@ function Explore() {
                     </DropdownButton> 
                 </div>
                 <div className='marketList'>
-                    {TotalObject.map(NFT=>(
-                    <Link to={`/Buy/${NFT.T_ID}`} style={{textDecoration: 'none', color:'black'}} state={{jsonAddress : NFT.T_URI, Cost : NFT.T_Price, Nid : NFT.T_ID}}>
-                        <NFTList NFT_url={NFT.T_URI} NFT_number={NFT.T_ID} NFT_price={NFT.T_Price}/>
-                    </Link>))}
+                    {nfts.map((nft, index) => (
+                        <>
+                        <Link to={`/Buy/${nfts[index].id}`} style={{textDecoration: 'none', color:'black'}} state={{jsonAddress : nfts[index].uri, Nid : nfts[index].id, Cost : nfts[index].price}}>
+                            <NFTList NFT_url={nfts[index].image} NFT_number={nfts[index].id} NFT_name={nfts[index].name} NFT_price={nfts[index].price}/>
+                        </Link>
+                        {/* <p>{nfts[index].image}</p> */}
+                        </>
+                    )
+                    )}
                 </div>
             </div>
             <div className='btmBar' fixed='bottom'>
